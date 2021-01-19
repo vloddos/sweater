774ad2281a2245fc79bd42c9269bb764de321cb1
vloddos <silver19987@gmail.com>
2021-01-19 20:00:54 +1000

    Add file uploading Minor fix login and register pages

--- src/main/java/org/example/sweater/controller/MainController.java
+++ src/main/java/org/example/sweater/controller/MainController.java
@@ -4,20 +4,31 @@ import org.example.sweater.domain.Message;
 import org.example.sweater.domain.User;
 import org.example.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -45,9 +56,30 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
+            @RequestParam("file") MultipartFile file,
             Map<String, Object> model
-    ) {
-        messageRepository.save(new Message(text, tag, user));
+    ) throws IOException {
+        var message = new Message(text, tag, user);
+
+        if (
+                file != null &&
+                        !Objects.requireNonNull(
+                                file.getOriginalFilename()
+                        ).isEmpty()
+        ) {
+            var uploadDir = new File(uploadPath);
+            if (!uploadDir.exists())
+                uploadDir.mkdir();
+
+            var filename = UUID.randomUUID().toString() + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath, filename));
+
+            message.setFilename(filename);
+        }
+
+        messageRepository.save(message);
+
         model.put("messages", messageRepository.findAll());
 
         return "main";
